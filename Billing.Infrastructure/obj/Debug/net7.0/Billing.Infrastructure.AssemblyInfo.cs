a9b8dc94cce918254799d08c9fc771cbbe906def
FABIO NEVES <fabioalvesneves@gmail.com>
2025-06-30 14:30:47 -0300

    project cache

--- Billing.Infrastructure/obj/Debug/net7.0/Billing.Infrastructure.AssemblyInfo.cs
+++ Billing.Infrastructure/obj/Debug/net7.0/Billing.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Billing.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5105ec5aa6d4892962a6bd1779062bd64e7e694")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec2dedb561aca1825e4cd0cfad8f34c691f954b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Billing.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Billing.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
